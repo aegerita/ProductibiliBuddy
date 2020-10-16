8d5a17764c9d39ca267a6804adaea6dad3910ce8
aegerita <jennytai3221@gmail.com>
2020-10-15 22:24:11 -0400

    title

--- src/main.js
+++ src/main.js
@@ -7,5 +7,8 @@ Vue.config.productionTip = false
 // load in the component that named "app"
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  mounted(){
+    document.title = 'Todos';
+  }
 }).$mount('#app')
